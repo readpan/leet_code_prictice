1c4ecbd0c03dbaea48af6233dd310e7a9f9fd997
readpan <readpan@gmail.com>
2019-09-11 18:31:04 +0800

    上传

--- LeetCodeReadPan/Easy/reverse_integer.cs
+++ LeetCodeReadPan/Easy/reverse_integer.cs
@@ -1,7 +1,57 @@
+using System;
+
 namespace LeetCodeReadPan.Easy
 {
+    /**
+     给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+    示例 1:
+
+    输入: 123
+    输出: 321
+     示例 2:
+
+    输入: -123
+    输出: -321
+    示例 3:
+
+    输入: 120
+    输出: 21
+    注意:
+
+    假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/reverse-integer
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     public class reverse_integer
     {
-        
+        public int Reverse(int x)
+        {
+            int result = 0;
+            string s = x.ToString();
+            if (x >= 0)
+            {
+                char[] cs = new char[s.Length];
+                for (int i = 0; i < s.Length; i++)
+                {
+                    cs[cs.Length - i - 1] = s[i];
+                }
+
+                result = int.Parse(new string(cs));
+            }
+            else
+            {
+                char[] cs = new char[s.Length - 1];
+                for (int i = 1; i < s.Length; i++)
+                {
+                    cs[cs.Length - i] = s[i];
+                }
+
+                result = -int.Parse(cs.ToString());
+            }
+
+            return result;
+        }
     }
 }
